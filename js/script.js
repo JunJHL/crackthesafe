1445d49cef5c75249845b21811447afd9b872ac4
Jesse <junjiejesse@gmail.com>
2018-10-19 01:23:45 +0000

    first day

--- js/script.js
+++ js/script.js
@@ -8,4 +8,16 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-
+$( document ).ready(function() {
+    $(".verify").click(function() {
+        var pw1=$("#number1").val();
+        var pw2=$("#number2").val();
+        var pw3=$("#number3").val();
+        var pw4=$("#number4").val();
+        var password=[pw1,pw2,pw3,pw4];
+        for(var i=0; i<password.length; i++) {
+            $("#userChoice").append(password[i]);
+        }
+    });
+    
+});
